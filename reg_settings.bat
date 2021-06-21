f13369d4931ac6f0ab0a8ec5ea97c11b8cdac542
R0ckNRolla <makarenko.e.v@gmail.com>
2021-06-21 22:32:57 +0300

    Update reg_settings.bat

--- reg_settings.bat
+++ reg_settings.bat
@@ -9,12 +9,14 @@ REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /V Us
 ::показывать значки в трее; требуеться перезапуск проводника
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /V TaskbarSmallIcons /T REG_DWORD /D 1 /F
 ::отключить автообновление винды; требуеться перезагузка винды
-REG ADD "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAutoUpdate" /V DisableWindowsUpdateAccess /T EG_DWORD /D 1 /F
+REG ADD "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\NoAutoUpdate" /V DisableWindowsUpdateAccess /T REG_DWORD /D 1 /F
 ::показывать скрытые файлы и папки
-reg add “HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced” /v Hidden /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
 ::отображать расширения файлов
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
 ::
 :: To kill and restart explorer
 taskkill /f /im explorer.exe
 start explorer.exe
+PAUSE 2
+EXIT
