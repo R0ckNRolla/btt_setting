ff07f74e5fe889fea8b4b3cee2d8ee32ee6ce64d
R0ckNRolla <makarenko.e.v@gmail.com>
2021-06-21 22:28:25 +0300

    Update reg_settings.bat

--- reg_settings.bat
+++ reg_settings.bat
@@ -13,7 +13,7 @@ REG ADD "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU
 ::показывать скрытые файлы и папки
 reg add “HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced” /v Hidden /t REG_DWORD /d 1 /f
 ::отображать расширения файлов
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 00000000 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
 ::
 :: To kill and restart explorer
 taskkill /f /im explorer.exe
